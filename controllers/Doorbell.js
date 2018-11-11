eed7daa2bee1ec606856f0f283ddd1e4994c8cf5
Nate <Ndelgado@student.neumont.edu>
2018-11-11 13:08:25 -0700

    testing audio playback

--- controllers/Doorbell.js
+++ controllers/Doorbell.js
@@ -5,7 +5,17 @@ class Doorbell {
   }
 
   ring() {
+    var sys = require("sys");
+    var exec = require("child_process").exec;
     this.load("./Data/doorbell/ring.mp3").then(this.play);
+    
+    exec("omxplayer -o local ./Data/doorbell/ring.mp3", function(
+      err,
+      stdout,
+      stderr
+    ) {
+      console.log(stdout);
+    });
   }
 }
 module.exports = Doorbell;
