59d01e417005e4edccdd5ef27027097e707afb31
Nate <Ndelgado@student.neumont.edu>
2018-11-14 09:45:15 -0700

    added basic routes

--- src/main.py
+++ src/main.py
@@ -5,5 +5,18 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello World!'
 
+@app.route('/lock')
+def lock():
+    return 'locking'
+
+@app.route('/unlock')
+def unlock():
+    return 'unlocking'
+    
+@app.route('/ring')
+def ring():
+    return 'ringing'
+
+    
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
