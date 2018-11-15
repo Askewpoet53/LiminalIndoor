648fa63ae10944c7bf87ed86b520627a0bd0637e
Nate <Ndelgado@student.neumont.edu>
2018-11-15 13:30:04 -0700

    testing servo

--- src/main.py
+++ src/main.py
@@ -1,46 +1,31 @@
 from flask import Flask
-import RPi.GPIO as GPIO
-
 app = Flask(__name__)
 
 
-@app.route("/")
+
+@app.route('/')
 def hello_world():
-    return "Hello World!"
+    return 'Hello World!'
 
-
-@app.route("/testServo")
+@app.route('/testServo')
 def testServo():
     print("testing servo...")
-    GPIO.setmode(GPIO.BOARD)
-    motorPin = 14
-    GPIO.setup(motorPin, GPIO.OUT)
-    motor = GPIO.PWM(motorPin, 50)
-    motor.start(0)  # Initialization
-    motor.ChangeDutyCycle(5)
-    motor.ChangeDutyCycle(7.5)
-    motor.ChangeDutyCycle(10)
-    motor.ChangeDutyCycle(12.5)
-    motor.ChangeDutyCycle(10)
-    motor.ChangeDutyCycle(7.5)
-    motor.ChangeDutyCycle(5)
-    motor.ChangeDutyCycle(2.5)
-    print("...Cleaning Resources")
-    motor.stop()
-    GPIO.cleanup()
-    return "testing"
+    from controllers.lock import lock
+    
+    lock = lock()
 
+    lock.test()
 
-@app.route("/unlock")
+    return 'testing'
+
+@app.route('/unlock')
 def unlock():
-    return "unlocking"
-
-
-@app.route("/ring")
+    return 'unlocking'
+    
+@app.route('/ring')
 def ring():
-    return "ringing"
+    return 'ringing'
 
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=80)
-
+    
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=80)
