d3576c6bb20b6e725f1a691c405671ccb7609a05
Nate <Ndelgado@student.neumont.edu>
2018-11-15 13:23:49 -0700

    testing servo

--- src/main.py
+++ src/main.py
@@ -1,26 +1,32 @@
 from flask import Flask
+
 app = Flask(__name__)
 
 
-
-@app.route('/')
+@app.route("/")
 def hello_world():
-    return 'Hello World!'
+    return "Hello World!"
 
-@app.route('/testServo')
+
+@app.route("/testServo")
 def testServo():
     print("testing servo...")
-    from controllers import test.py
-    return 'testing'
+    from controllers import test
 
-@app.route('/unlock')
+    test.run()
+    return "testing"
+
+
+@app.route("/unlock")
 def unlock():
-    return 'unlocking'
-    
-@app.route('/ring')
-def ring():
-    return 'ringing'
+    return "unlocking"
 
-    
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80)
+
+@app.route("/ring")
+def ring():
+    return "ringing"
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=80)
+
