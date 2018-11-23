331d1543f1ec9d77cc00d27c98132b72410afb04
Nate <Ndelgado@student.neumont.edu>
2018-11-23 12:07:06 -0700

    added doorbell route

--- src/main.py
+++ src/main.py
@@ -13,7 +13,12 @@ def hello_world():
 
 @app.route("/doorbell")
 def doorbell():
-    subprocess.run("raspistill -o peephole");
+    with picamera.PiCamera() as camera:
+        camera.resolution = (1024, 768)
+        camera.start_preview()
+        # Camera warm-up time
+        time.sleep(2)
+        camera.capture('peephole.jpg')
     return "doorbell"
 
 @app.route("/unlock")
